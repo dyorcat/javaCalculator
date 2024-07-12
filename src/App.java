8e045c2a35425c4fde845f8c2e0e48da0fd504bd
dyorcat <yool0417@gmail.com>
2024-07-12 20:43:38 +0900

    lv2 10

--- src/App.java
+++ src/App.java
@@ -1,41 +1,57 @@
 import java.util.*;
 
 public class App {
-    public static void main(String[] args) {
 
-        Calculator calculator = new Calculator(new ArrayList<>());
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
-                System.out.print("첫 번째 숫자를 입력하세요.");
-                int num1 = sc.nextInt();
+        ArithmeticCalculator arithmeticCalculator = new ArithmeticCalculator(new ArrayList<>());
+        CircleCalculator circleCalculator = new CircleCalculator(new ArrayList<>());
 
-                System.out.print("두 번째 숫자를 입력하세요.");
+        do {
+
+            System.out.println("어떤 계산을 하시겠습니까? (1: 사칙연산, 2: 원의 넓이)");
+            int choice = sc.nextInt();
+
+            if(choice == 1) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int num1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
                 int num2 = sc.nextInt();
 
-                System.out.print("사칙연산 기호를 입력하세요. (+, -, *, /)");
+                System.out.print("사칙연산 기호를 입력하세요: (+, -, *, /");
+
                 char operator = sc.next().charAt(0);
 
-                double result = calculator.calculate(num1, num2, operator);
-                System.out.println("결과 : " + result);
+                double result = arithmeticCalculator.calculate(num1, num2, operator);
+                arithmeticCalculator.getResults().add(result);
 
-                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (y/n) : ");
-                String removeAnswer = sc. next();
-                if (removeAnswer.equalsIgnoreCase("y")) {
-                    calculator.removeResult(0);
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (y / n)");
+                if (Objects.equals(sc.next(), "y")) {
+                    arithmeticCalculator.removeResult(0);
                 }
 
-                System.out.print("연산 기록을 조회하시겠습니까? (y/n): ");
-                String historyAnswer = sc.next();
-                if (historyAnswer.equalsIgnoreCase("y")) {
-                    System.out.println(calculator.getOperationHistory());
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (y / n)");
+                if (Objects.equals(sc.next(), "y")) {
+                    arithmeticCalculator.inquiryResults();
                 }
+            } else {
+                System.out.println("원의 반지름을 입력하세요: ");
+                int radius = sc.nextInt();
 
-                System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
-                String exitInput = sc.next();
-                if (exitInput.equalsIgnoreCase("exit")) {
-                    break;
+                double area = circleCalculator.calculate(radius);
+                circleCalculator.getResults().add(area);
+                System.out.println("반지름이 " + radius + "인 원의 넓이는 : " + area);
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (y / n)");
+                if (Objects.equals(sc.next(), "y")) {
+                    System.out.println("저장된 원의 넓이 전체 조회: ");
+                    circleCalculator.inquiryResults();
                 }
-        }
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+        } while (!sc.next().equals("exit"));
+
     }
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,48 +1,23 @@
-import java.util.ArrayList;
 import java.util.List;
-public class Calculator {
-    private List<String> results;
 
-    public Calculator(List<String> results) {
+public abstract class Calculator {
+    public static final double PI = 3.14;
+
+    // 연산 결과들 저장된 리스트 선언
+    private final List<Double> results;
+
+
+    public Calculator(List<Double> results) {
         this.results = results;
-    }
-    public double calculate(int num1, int num2, char operator)  {
-        int result = 0;
-        String operation = num1 + " " + operator + " " + num2;
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 == 0) {
-                    throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                }
-                result = num1 / num2;
-                break;
-            default:
-                throw new UnsupportedOperationException("올바른 연산자가 아닙니다.");
-        }
-
-        addToOperationHistory(operation + " = " + result);
-        return result;
-    }
-
-    public List<String> getOperationHistory() {
-        return results;
-    }
-
-    private void addToOperationHistory(String entry) {
-        results.add(entry);
     }
 
     public void removeResult(int index) {
         this.results.remove(index);
+    }
+
+    abstract void inquiryResults();
+
+    public List<Double> getResults() {
+        return results;
     }
 }
